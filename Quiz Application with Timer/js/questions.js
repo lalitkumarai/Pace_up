7a6f5aa76dded383e9b1961ef9d4f37469b89fce
Lalit kumar <lalitrajput79026@gmail.com>
2025-03-18 15:08:52 +0530

    files add

---
+++ Quiz Application with Timer/js/questions.js
@@ -0,0 +1,115 @@
+// creating an array and passing the number, questions, options, and answers
+let questions = [
+    {
+    numb: 1,
+    question: "What comes next in the series: 3, 6, 9, 12, ___?",
+    answer: "18",
+    options: [
+      "15",
+      "18",
+      "21",
+      "24"
+    ]
+  },
+    {
+    numb: 2,
+    question: "Which shape is missing from the pattern: ▲ ▲ ▲ ▼ ▲ ▲ ▲ ___?",
+    answer: "▼",
+    options: [
+      "▼",
+      "▲",
+      "■",
+      "●"
+    ]
+  },
+    {
+    numb: 3,
+    question: " Fill in the missing number: 2, 4, 8, 16, ___.",
+    answer: "32",
+    options: [
+      "24",
+      "32",
+      "48",
+      "64"
+    ]
+  },
+    {
+    numb: 4,
+    question: "What is the next letter in the sequence: A, C, E, G, ___?",
+    answer: "I",
+    options: [
+      "H",
+      "I",
+      "J",
+      "K"
+    ]
+  },
+    {
+    numb: 5,
+    question: "If 'book' is to 'reading' then 'fork' is to ____?",
+    answer: "eating",
+    options: [
+      "eating",
+      "food",
+      "kitchen",
+      "cooking"
+    ]
+  },
+  // you can uncomment the below codes and make duplicate as more as you want to add question
+  // but remember you need to give the numb value serialize like 1,2,3,5,6,7,8,9.....
+
+   {
+  numb: 6,
+  question: "Complete the analogy: Sun is to day as moon is to ____?",
+  answer: "night",
+  options: [
+    "night",
+    "star",
+     "sky",
+    "dark"
+  ] },
+  {
+    numb: 7,
+    question: "If 'run' is to 'ran' then 'sing' is to ____?",
+    answer: "sang",
+    options: [
+      "sung",
+      "sang",
+      "singed",
+      "sings"
+    ]
+  },
+  {
+    numb: 8,
+    question: "If 'circle' is to 'round' then 'square' is to ____?",
+    answer:"corner",
+    options: [
+      "sharp",
+      "triangle",
+      "rectangle",
+      "corner"
+    ]
+  },
+  {
+    numb: 9,
+    question: "If all bloops are razzies and all razzies are lazzies, then are all bloops definitely lazzies?",
+    answer: "yes",
+    options: [
+      "yes",
+      "no"
+      
+    ]
+  },
+  {
+    numb: 10,
+    question: "Mary's father has five daughters: 1. Nana, 2. Nene, 3. Nini, 4. Nono. What is the fifth daughter's name?",
+    answer: "Mary",
+    options: [
+      "Nunu",
+      "Mary",
+      "Lisa",
+      "Nina"
+    ]
+  },
+
+];
